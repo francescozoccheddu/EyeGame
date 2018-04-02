a446b33a8f5065b284bd02772a33b8627a08ae44
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2018-04-02 14:56:45 +0200

    Diffuse lighting setup

--- Workbench/default_vertex.glsl
+++ Workbench/default_vertex.glsl
@@ -1,15 +1,15 @@
-#version 150 core
+#version 130 core
 
 in vec3 aPosition;
 in vec3 aNormal;
 uniform mat4 uProjView;
 uniform vec3 uLightPos;
-out vec3 vColor;
+out vec3 vLight;
+out vec3 vNormal;
 
 void main()
 {
-	vec3 s2l = uLightPos - aPosition;
-	float lambert = dot(s2l,aNormal);
-	vColor = vec3(1.0) * lambert;
+	vNormal = aNormal;
+	vLight = uLightPos - aPosition;
 	gl_Position = uProjView * vec4(aPosition, 1.0);
 }
